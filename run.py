ff5bbedbd6e528e9af00aab06d6434ff883bcc27
tiancailin <linweiming@hotmail.com>
2023-11-28 15:53:24 +0800

    compute data complate

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import permutation_importance
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import joblib
+
+# 文件保存实际路径
+save_root = "xls分析202311/"
+split_csv = save_root + "processed_allData.csv"
+reslut_PDF01 = save_root + "2023.sample.RF.Validation.pdf"
+reslut_PDF02 = save_root + "2023.sample.RF.Importance.pdf"
 
 # 读取csv文件
 # df = pd.read_csv('test.csv', comment='#', na_values='无此项', header=0)
@@ -35,8 +43,8 @@ df_numeric = df.select_dtypes(include=np.number).copy()
 print('Number of rows:', len(df_numeric))
 
 # print(df_numeric)
-# 只选取2018年的数据
-# df_numeric = df_numeric[df_numeric["Year"] == 2018]
+# 只选取特定年的数据
+df_numeric = df_numeric[df_numeric["Year"] == 2023]
 # 删除Year列 
 df_numeric.drop(columns=['Year'], inplace=True)
 
@@ -56,11 +64,150 @@ print('最终可训练数据:', len(df_numeric))
 # print(missing_count)
 # print(na_count)
 
-# save to excel
-df_numeric.to_excel("output.xlsx", index=False)  # For xlsx format
+# 2023.11.27开始添加的重要内容，完全实现李博R语言部分的功能
+# save to csv，从这里开始
+# df_numeric.to_csv("output_20231127.csv", index=False)  # For xlsx csv
+
+# 写入CSV文件
+df_numeric.to_csv(split_csv, index=False)
+
+# 设置随机数种子
+np.random.seed(123)
+
+# 划分训练集
+samples = np.random.choice(df_numeric.index, size=int(np.ceil(len(df_numeric) * 0.7)), replace=False)
+train_data = df_numeric.loc[samples]
+
+# 构建随机森林模型
+rf_model = RandomForestRegressor(n_estimators=500, random_state=123)
+rf_model.fit(train_data.drop(columns='Score'), train_data['Score'])
+
+# 输出随机森林模型，并保存
+print(rf_model)
+joblib.dump(rf_model, save_root + 'model_20231127.pkl')
+
+# 在训练集上进行预测
+train_predictions = rf_model.predict(train_data.drop(columns='Score'))
+
+# 绘制训练集图表
+plt.figure(figsize=(9, 6))
+plt.subplot(1, 2, 1)
+plt.scatter(train_data['Score'], train_predictions)
+plt.title('Train')
+plt.xlabel('Score')
+plt.ylabel('Predict')
+plt.plot([0, 100], [0, 100], color='red')
+plt.text(15, 85, f"R={np.corrcoef(train_data['Score'], train_predictions)[0, 1]:.2f}", color='blue')
+
+# 在测试集上进行预测
+# test_predictions = rf_model.predict(df_numeric.drop(samples)['Score']) # 之前报错的
+test_predictions = rf_model.predict(df_numeric.drop(samples).drop(columns='Score'))
+
+# 绘制测试集图表
+plt.subplot(1, 2, 2)
+plt.scatter(df_numeric.drop(samples)['Score'], test_predictions)
+plt.title('Test')
+plt.xlabel('Score')
+plt.ylabel('Predict')
+plt.plot([0, 100], [0, 100], color='red')
+plt.text(15, 85, f"R={np.corrcoef(df_numeric.drop(samples)['Score'], test_predictions)[0, 1]:.2f}", color='blue')
+
+# 保存PDF图表
+plt.savefig(reslut_PDF01)
+plt.show()
+
+# 计算 Gini Importance
+gini_importance = rf_model.feature_importances_
+
+# 计算均方误差的减少
+mse_reduction = []
+for feature in train_data.drop(columns='Score').columns:
+    X_permuted = train_data.drop(columns='Score').copy()
+    X_permuted[feature] = np.random.permutation(X_permuted[feature])
+    mse_permuted = ((rf_model.predict(X_permuted) - train_data['Score']) ** 2).mean()
+    mse_reduction.append(mse_permuted)
+
+# 计算排列重要性
+perm_importance = permutation_importance(rf_model, train_data.drop(columns='Score'), train_data['Score'], n_repeats=30, random_state=123)
+perm_importance_mean = perm_importance.importances_mean
+
+# 提取变量重要性
+importance_otu = pd.DataFrame({
+    'Feature': train_data.drop(columns='Score').columns,
+    'Gini_Importance': gini_importance,
+    'MSE_Reduction': mse_reduction,
+    'Permutation_Importance': perm_importance_mean
+})
+
+# 按照 Gini Importance 从大到小排序
+# importance_otu = importance_otu.sort_values(by='Gini_Importance', ascending=False)
+
+# 分别按照三种重要性从大到小排序
+importance_otu_gini = importance_otu.sort_values(by='Gini_Importance', ascending=False)
+importance_otu_mse = importance_otu.sort_values(by='MSE_Reduction', ascending=False)
+importance_otu_permutation = importance_otu.sort_values(by='Permutation_Importance', ascending=False)
+print(importance_otu_gini)
+print(importance_otu_mse)
+print(importance_otu_permutation)
+# 输出前几行变量重要性
+print(importance_otu.head())
+
+# 保存变量重要性图表为PDF
+plt.figure(figsize=(12, 24)) # 调整图表大小
+# plt.title('Variable Importance')
+
+# Gini Importance
+plt.subplot(1, 3, 1)
+plt.title('Gini Importance')
+plt.barh(importance_otu_gini['Feature'][:30], importance_otu_gini['Gini_Importance'][:30][::-1])
+
+# MSE Reduction
+plt.subplot(1, 3, 2)
+plt.title('MSE Reduction')
+plt.barh(importance_otu_mse['Feature'][:30], importance_otu_mse['MSE_Reduction'][:30][::-1], color='orange')
+
+# Permutation Importance
+plt.subplot(1, 3, 3)
+plt.title('Permutation Importance')
+plt.barh(importance_otu_permutation['Feature'][:30], importance_otu_permutation['Permutation_Importance'][:30][::-1], color='green')
+
+plt.tight_layout()  # 调整布局以避免重叠
+plt.savefig(reslut_PDF02)
+plt.show()
+
+
+"""
+# 之前跑通的，到结束
+# 提取变量重要性
+importance_otu = pd.DataFrame({'Feature': df_numeric.drop(columns='Score').columns, 'Importance': rf_model.feature_importances_})
+
+# 按照重要性排序
+importance_otu = importance_otu.sort_values(by='Importance', ascending=False)
+
+# 输出前几行变量重要性
+print(importance_otu.head())
+
+# 保存变量重要性图表为PDF
+plt.figure(figsize=(9, 6))
+plt.title('Variable Importance')
+
+# 交换 x 和 y 轴，并按重要性从大到小排列
+# plt.barh(importance_otu['Feature'][:30], importance_otu['Importance'][:30])
+plt.barh(importance_otu['Feature'][:30][::-1], importance_otu['Importance'][:30][::-1])
+plt.savefig(reslut_PDF02)
+plt.show()
+"""
 
 
 
+
+
+
+"""
+以下是2023.5月份前的旧代码不要的部分，暂时保存，并移动至文件尾部
+紧跟print('最终可训练数据:', len(df_numeric))
+
+df_numeric.to_csv("output_20231127.csv", index=False)  # For xlsx csv
 
 # 拆分训练集和测试集
 # train_indices = np.random.rand(len(df_numeric)) < 0.7
@@ -93,3 +240,4 @@ plt.show()
 # 将模型结果保存至文件
 import joblib
 joblib.dump(rf, 'model_20231124.pkl')
+"""
