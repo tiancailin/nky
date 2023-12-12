97406b59aecbdc6ec08560e79e3755f61efc23f9
tiancailin <linweiming@hotmail.com>
2023-12-12 11:46:24 +0800

    compute right result

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import joblib
 
-# 文件保存实际路径
+# 文件保存实际路径 & 运行前可修改项
 save_root = "xls分析202311/"
 split_csv = save_root + "processed_allData.csv"
 reslut_PDF01 = save_root + "2023.sample.RF.Validation.pdf"
 reslut_PDF02 = save_root + "2023.sample.RF.Importance.pdf"
-
+Year = 2023
 # 读取csv文件
 # df = pd.read_csv('test.csv', comment='#', na_values='无此项', header=0)
 df = pd.read_csv('2018-2020年考核单位的得分情况(2023.11）-分析.csv', comment='#', na_values='无此项', header=0, low_memory=False)
@@ -43,9 +43,8 @@ df_numeric = df.select_dtypes(include=np.number).copy()
 print('Number of rows:', len(df_numeric))
 
 # print(df_numeric)
-# 只选取特定年的数据
-df_numeric = df_numeric[df_numeric["Year"] == 2019]
-# 删除Year列 
+# 只选取特定年的数据,再删除该列
+df_numeric = df_numeric[df_numeric["Year"] == Year]
 df_numeric.drop(columns=['Year'], inplace=True)
 
 print('Number of 训练数据:', len(df_numeric))
@@ -114,10 +113,10 @@ plt.text(15, 85, f"R={np.corrcoef(df_numeric.drop(samples)['Score'], test_predic
 
 # 保存PDF图表
 # plt.savefig(reslut_PDF01)
-# plt.show()
+plt.show()
 
 # 下面是重要性排序的计算
-
+# 使用默认的feature_importances_
 # 提取变量重要性
 importance_otu = pd.DataFrame({'Feature': df_numeric.drop(columns='Score').columns, 'Importance': rf_model.feature_importances_})
 
@@ -125,25 +124,24 @@ importance_otu = pd.DataFrame({'Feature': df_numeric.drop(columns='Score').colum
 importance_otu = importance_otu.sort_values(by='Importance', ascending=False)
 
 # 输出前几行变量重要性
-print(importance_otu.head(20))
+print(importance_otu.head(5))
 
-# 保存变量重要性图表为PDF
+# visualize部分
 plt.figure(figsize=(9, 6))
 plt.title('Variable Importance')
 
 # 交换 x 和 y 轴，并按重要性从大到小排列
-# plt.barh(importance_otu['Feature'][:30], importance_otu['Importance'][:30])
-plt.barh(importance_otu['Feature'][:30][::-1], importance_otu['Importance'][:30][::-1])
+plt.barh(importance_otu['Feature'][:20][::-1], importance_otu['Importance'][:20][::-1])
 # plt.savefig(reslut_PDF02)
-# plt.show()
+plt.show()
 
 
 
-"""
-三联gini、mse、perm重要性，先往后放放
-2023.12.11
+# 三联gini、mse、perm重要性，先往后放放
+# 2023.12.11
 # 计算 Gini Importance
 gini_importance = rf_model.feature_importances_
+# gini_importance = pd.DataFrame({'Feature': df_numeric.drop(columns='Score').columns, 'Importance': rf_model.feature_importances_})
 
 # 计算均方误差的减少
 mse_reduction = []
@@ -154,53 +152,54 @@ for feature in train_data.drop(columns='Score').columns:
     mse_reduction.append(mse_permuted)
 
 # 计算排列重要性
-perm_importance = permutation_importance(rf_model, train_data.drop(columns='Score'), train_data['Score'], n_repeats=30, random_state=123)
-perm_importance_mean = perm_importance.importances_mean
+# perm_importance = permutation_importance(rf_model, train_data.drop(columns='Score'), train_data['Score'], n_repeats=30, random_state=123)
+# perm_importance_mean = perm_importance.importances_mean
 
 # 提取变量重要性
-importance_otu = pd.DataFrame({
+importance_otu_all = pd.DataFrame({
     'Feature': train_data.drop(columns='Score').columns,
     'Gini_Importance': gini_importance,
-    'MSE_Reduction': mse_reduction,
-    'Permutation_Importance': perm_importance_mean
+    'MSE_Reduction': mse_reduction
+    # 'Permutation_Importance': perm_importance_mean
 })
 
 # 按照 Gini Importance 从大到小排序
 # importance_otu = importance_otu.sort_values(by='Gini_Importance', ascending=False)
 
 # 分别按照三种重要性从大到小排序
-importance_otu_gini = importance_otu.sort_values(by='Gini_Importance', ascending=False)
-importance_otu_mse = importance_otu.sort_values(by='MSE_Reduction', ascending=False)
-importance_otu_permutation = importance_otu.sort_values(by='Permutation_Importance', ascending=False)
+importance_otu_gini = importance_otu_all.sort_values(by='Gini_Importance', ascending=False)
+importance_otu_mse = importance_otu_all.sort_values(by='MSE_Reduction', ascending=False)
+# importance_otu_permutation = importance_otu_all.sort_values(by='Permutation_Importance', ascending=False)
 print(importance_otu_gini)
 print(importance_otu_mse)
-print(importance_otu_permutation)
+# print(importance_otu_permutation)
 # 输出前几行变量重要性
-print(importance_otu.head())
+print(importance_otu_all.head())
 
 # 保存变量重要性图表为PDF
 plt.figure(figsize=(12, 24)) # 调整图表大小
 # plt.title('Variable Importance')
 
 # Gini Importance
-plt.subplot(1, 3, 1)
+plt.subplot(1, 2, 1)
 plt.title('Gini Importance')
-plt.barh(importance_otu_gini['Feature'][:30], importance_otu_gini['Gini_Importance'][:30][::-1])
+plt.barh(importance_otu_gini['Feature'][:30][::-1], importance_otu_gini['Gini_Importance'][:30][::-1])
 
 # MSE Reduction
-plt.subplot(1, 3, 2)
+plt.subplot(1, 2, 2)
 plt.title('MSE Reduction')
-plt.barh(importance_otu_mse['Feature'][:30], importance_otu_mse['MSE_Reduction'][:30][::-1], color='orange')
+plt.barh(importance_otu_mse['Feature'][:30][::-1], importance_otu_mse['MSE_Reduction'][:30][::-1], color='orange')
 
 # Permutation Importance
-plt.subplot(1, 3, 3)
-plt.title('Permutation Importance')
-plt.barh(importance_otu_permutation['Feature'][:30], importance_otu_permutation['Permutation_Importance'][:30][::-1], color='green')
+# temp commet
+# plt.subplot(1, 3, 3)
+# plt.title('Permutation Importance')
+# plt.barh(importance_otu_permutation['Feature'][:30][::-1], importance_otu_permutation['Permutation_Importance'][:30][::-1], color='green')
 
 plt.tight_layout()  # 调整布局以避免重叠
-plt.savefig(reslut_PDF02)
+# plt.savefig(reslut_PDF02)
 plt.show()
-"""
+
 
 
 
